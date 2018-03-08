b5fb9cb55cde46b85b188414274d6a23755be435
camander321 <chamburg321@gmail.com>
2018-03-08 09:20:51 -0800

    update sql file

--- mjbs.sql
+++ mjbs.sql
@@ -2,8 +2,8 @@
 -- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 05, 2018 at 11:36 PM
+-- Host: localhost:8889
+-- Generation Time: Mar 08, 2018 at 05:18 PM
 -- Server version: 5.6.34-log
 -- PHP Version: 7.1.5
 
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `mjbs`
 --
-CREATE DATABASE IF NOT EXISTS `mjbs` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `mjbs` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `mjbs`;
 
 -- --------------------------------------------------------
@@ -41,30 +41,6 @@ CREATE TABLE `battle_text` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inventory`
---
-
-CREATE TABLE `inventory` (
-  `id` int(11) NOT NULL,
-  `team_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `items`
---
-
-CREATE TABLE `items` (
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `players`
 --
 
@@ -73,37 +49,15 @@ CREATE TABLE `players` (
   `name` varchar(255) NOT NULL,
   `hp_total` int(11) NOT NULL,
   `hp_remaining` int(11) NOT NULL,
-  `flavor_id` int(11),
+  `flavor_id` int(11) DEFAULT NULL,
   `agility` int(11) NOT NULL,
   `intelligence` int(11) NOT NULL,
   `strength` int(11) NOT NULL,
-  `luck` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `players_teams`
---
-
-CREATE TABLE `players_teams` (
-  `id` int(11) NOT NULL,
-  `player_id` int(11) NOT NULL,
-  `team_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teams`
---
-
-CREATE TABLE `teams` (
-  `name` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL,
-  `coordinates` varchar(255) NOT NULL,
-  `allegiance` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `luck` int(11) NOT NULL,
+  `allegience` tinyint(1) NOT NULL,
+  `x_pos` int(11) DEFAULT NULL,
+  `y_pos` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
 -- Indexes for dumped tables
@@ -116,39 +70,11 @@ ALTER TABLE `battle_text`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `inventory`
---
-ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `inventory_fk0` (`team_id`),
-  ADD KEY `inventory_fk1` (`item_id`);
-
---
--- Indexes for table `items`
---
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `players`
 --
 ALTER TABLE `players`
   ADD PRIMARY KEY (`id`),
   ADD KEY `players_fk0` (`flavor_id`);
-
---
--- Indexes for table `players_teams`
---
-ALTER TABLE `players_teams`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `players_teams_fk0` (`player_id`),
-  ADD KEY `players_teams_fk1` (`team_id`);
-
---
--- Indexes for table `teams`
---
-ALTER TABLE `teams`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -160,279 +86,19 @@ ALTER TABLE `teams`
 ALTER TABLE `battle_text`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `inventory`
---
-ALTER TABLE `inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `items`
---
-ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `players`
 --
 ALTER TABLE `players`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `players_teams`
---
-ALTER TABLE `players_teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `teams`
---
-ALTER TABLE `teams`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `inventory`
---
-ALTER TABLE `inventory`
-  ADD CONSTRAINT `inventory_fk0` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `inventory_fk1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE;
-
---
 -- Constraints for table `players`
 --
 ALTER TABLE `players`
   ADD CONSTRAINT `players_fk0` FOREIGN KEY (`flavor_id`) REFERENCES `battle_text` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `players_teams`
---
-ALTER TABLE `players_teams`
-  ADD CONSTRAINT `players_teams_fk0` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `players_teams_fk1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
-
-
-
-
-
-
-
-
-
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Mar 05, 2018 at 11:36 PM
--- Server version: 5.6.34-log
--- PHP Version: 7.1.5
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `mjbs`
---
-CREATE DATABASE IF NOT EXISTS `mjbs_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `mjbs_test`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `battle_text`
---
-
-CREATE TABLE `battle_text` (
-  `id` int(11) NOT NULL,
-  `start_battle` varchar(255) NOT NULL,
-  `end_battle` varchar(255) NOT NULL,
-  `weakened_response` varchar(255) NOT NULL,
-  `post_response` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `inventory`
---
-
-CREATE TABLE `inventory` (
-  `id` int(11) NOT NULL,
-  `team_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `items`
---
-
-CREATE TABLE `items` (
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `players`
---
-
-CREATE TABLE `players` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `hp_total` int(11) NOT NULL,
-  `hp_remaining` int(11) NOT NULL,
-  `flavor_id` int(11),
-  `agility` int(11) NOT NULL,
-  `intelligence` int(11) NOT NULL,
-  `strength` int(11) NOT NULL,
-  `luck` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `players_teams`
---
-
-CREATE TABLE `players_teams` (
-  `id` int(11) NOT NULL,
-  `player_id` int(11) NOT NULL,
-  `team_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teams`
---
-
-CREATE TABLE `teams` (
-  `name` varchar(255) NOT NULL,
-  `id` int(11) NOT NULL,
-  `coordinates` varchar(255) NOT NULL,
-  `allegiance` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `battle_text`
---
-ALTER TABLE `battle_text`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `inventory`
---
-ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `inventory_fk0` (`team_id`),
-  ADD KEY `inventory_fk1` (`item_id`);
-
---
--- Indexes for table `items`
---
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `players`
---
-ALTER TABLE `players`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `players_fk0` (`flavor_id`);
-
---
--- Indexes for table `players_teams`
---
-ALTER TABLE `players_teams`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `players_teams_fk0` (`player_id`),
-  ADD KEY `players_teams_fk1` (`team_id`);
-
---
--- Indexes for table `teams`
---
-ALTER TABLE `teams`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `battle_text`
---
-ALTER TABLE `battle_text`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `inventory`
---
-ALTER TABLE `inventory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `items`
---
-ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `players`
---
-ALTER TABLE `players`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `players_teams`
---
-ALTER TABLE `players_teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `teams`
---
-ALTER TABLE `teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `inventory`
---
-ALTER TABLE `inventory`
-  ADD CONSTRAINT `inventory_fk0` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `inventory_fk1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `players`
---
-ALTER TABLE `players`
-  ADD CONSTRAINT `players_fk0` FOREIGN KEY (`flavor_id`) REFERENCES `battle_text` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `players_teams`
---
-ALTER TABLE `players_teams`
-  ADD CONSTRAINT `players_teams_fk0` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `players_teams_fk1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
